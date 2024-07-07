2fa68caa2494dadc8f7ea4c0ace27eaf65d06354
OkAndGreat <2238091763@qq.com>
2024-07-08 02:42:37 +0800

    完成首页列表item编写

--- src/pages/home/component/HomeListItem.js
+++ src/pages/home/component/HomeListItem.js
@@ -10,12 +10,27 @@ export class HomeListItem extends PureComponent {
     render() {
         const {data} = this.props
 
+        const authorStyle = [
+            styles.author,
+            data.niceShareDate.includes("前") ? {marginStart: 5} : null,
+        ].filter(style => style)
+
         return <View style={styles.container}>
-            <View style={styles.topContainer}></View>
+            <View style={styles.topContainer}>
+                <View style={styles.topContainerLeft}>
+                    {data.niceShareDate.includes("前") ? (
+                        <Text>新</Text>
+                    ) : null}
+                    <Text style={authorStyle}>{data.author ? data.author : data.shareUser}</Text>
+                </View>
+                <Text>{data.niceShareDate}</Text>
+            </View>
             <View style={styles.centerContainer}>
                 <Text>{data.title}</Text>
             </View>
-            <View style={styles.bottomContainer}></View>
+            <View style={styles.bottomContainer}>
+                <Text style={styles.chapter}>{data.superChapterName + "·" + data.chapterName}</Text>
+            </View>
         </View>;
     }
 }
@@ -23,13 +38,21 @@ export class HomeListItem extends PureComponent {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: "column"
-
+        flexDirection: "column",
+        marginStart: 12,
+        marginEnd: 12,
+        marginBottom:20
+    },
+    topContainerLeft: {
+        flexDirection: "row",
     },
     topContainer: {
         flexDirection: "row",
         justifyContent: "space-between",
     },
     centerContainer: {},
-    bottomContainer: {flexDirection: "row"}
+    bottomContainer: {flexDirection: "row"},
+    author: {},
+    niceShareDate: {},
+    chapter: {}
 })
